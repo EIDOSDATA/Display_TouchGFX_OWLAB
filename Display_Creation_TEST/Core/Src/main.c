a8e1056d3d55f43afdef04982f1df36910673166
EIDOS <ihjisan00@gmail.com>
2025-03-19 17:17:36 +0900

    SD CARD ADD
    SD CARD ADD

--- Display_Creation_TEST/Core/Src/main.c
+++ Display_Creation_TEST/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "fatfs.h"
 #include "app_touchgfx.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -72,6 +73,8 @@ JPEG_HandleTypeDef hjpeg;
 
 LTDC_HandleTypeDef hltdc;
 
+SD_HandleTypeDef hsd1;
+
 TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
@@ -95,6 +98,7 @@ static void MX_TIM13_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_TIM3_Init(void);
 static void MX_TIM4_Init(void);
+static void MX_SDMMC1_SD_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -143,6 +147,8 @@ int main(void)
 	MX_TIM1_Init();
 	MX_TIM3_Init();
 	MX_TIM4_Init();
+	MX_SDMMC1_SD_Init();
+	MX_FATFS_Init();
 	MX_TouchGFX_Init();
 	/* USER CODE BEGIN 2 */
 
@@ -224,7 +230,7 @@ void SystemClock_Config(void)
 	RCC_OscInitStruct.PLL.PLLM = 2;
 	RCC_OscInitStruct.PLL.PLLN = 64;
 	RCC_OscInitStruct.PLL.PLLP = 2;
-	RCC_OscInitStruct.PLL.PLLQ = 2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
 	RCC_OscInitStruct.PLL.PLLR = 2;
 	RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
 	RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
@@ -456,6 +462,33 @@ static void MX_LTDC_Init(void)
 	/* USER CODE BEGIN LTDC_Init 2 */
 
 	/* USER CODE END LTDC_Init 2 */
+
+}
+
+/**
+ * @brief SDMMC1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SDMMC1_SD_Init(void)
+{
+
+	/* USER CODE BEGIN SDMMC1_Init 0 */
+
+	/* USER CODE END SDMMC1_Init 0 */
+
+	/* USER CODE BEGIN SDMMC1_Init 1 */
+
+	/* USER CODE END SDMMC1_Init 1 */
+	hsd1.Instance = SDMMC1;
+	hsd1.Init.ClockEdge = SDMMC_CLOCK_EDGE_RISING;
+	hsd1.Init.ClockPowerSave = SDMMC_CLOCK_POWER_SAVE_DISABLE;
+	hsd1.Init.BusWide = SDMMC_BUS_WIDE_4B;
+	hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
+	hsd1.Init.ClockDiv = 16;
+	/* USER CODE BEGIN SDMMC1_Init 2 */
+
+	/* USER CODE END SDMMC1_Init 2 */
 
 }
 
--- Display_Creation_TEST/Debug/sources.mk
+++ Display_Creation_TEST/Debug/sources.mk
@@ -46,6 +46,10 @@ Core/Src/User_App/Memory \
 Core/Src \
 Core/Startup \
 Drivers/STM32H7xx_HAL_Driver/Src \
+FATFS/App \
+FATFS/Target \
+Middlewares/Third_Party/FatFs/src \
+Middlewares/Third_Party/FatFs/src/option \
 TouchGFX/App \
 TouchGFX/build/MINGW32_NT-6.2/generated/fonts/src \
 TouchGFX/build/MINGW32_NT-6.2/generated/gui_generated/src/common \
